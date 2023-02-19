f783873480db04eb2a360db300ba4895410f570a
kuldeepsinh1 <kuldeep.mahida@icloud.com>
2023-02-19 11:41:36 -0500

    file check

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: kuldeepsinh sandipsinh mahida Student ID: 167547215 Date: 3 feb 2023
+*
+*  Online (Cyclic) Link: https://real-gray-harp-seal-hose.cyclic.app
+*
+********************************************************************************/ 
+
 var express = require("express");
 var path = require("path");
 var app = express();
